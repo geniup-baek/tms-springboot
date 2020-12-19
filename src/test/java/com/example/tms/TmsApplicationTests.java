8b3b59e0c31182d815424a0684bf9a2b97feacce
Geniup Baek <geniup.baek@gmail.com>
2020-12-19 21:11:07 +0900

    comment out empty @Test

--- src/test/java/com/example/tms/TmsApplicationTests.java
+++ src/test/java/com/example/tms/TmsApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.tms;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TmsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
